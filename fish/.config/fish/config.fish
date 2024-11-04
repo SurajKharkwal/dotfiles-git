b87441da8e57734656f9e6efc53b972c704fb97f
= <=>
2024-11-04 00:10:00 +0000

    Add : neofetch
    neofetch to exec for every first istance of fish after every restart

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,14 +2,15 @@ set -g fish_greeting ''
 
 starship init fish | source
 
-if not test -f /tmp/neofetch_shown
+if not test -f /tmp/neofetch_flag.txt
     # Show Neofetch if the file does not exist
     neofetch
     # Create the file to indicate Neofetch has been shown
-    touch /tmp/neofetch_shown
+    touch /tmp/neofetch_flag.txt
 end
 alias nv="nvim"
 alias lg="lazygit"
+alias yy="yazi"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
